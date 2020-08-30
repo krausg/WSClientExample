aa23f5771258c51022cb1831f56a575c6ec79157
krausg <guenter.kraus@mailbox.org>
2020-08-30 02:42:42 +0200

    code angepasst, damit man username und passwort angeben kann als wssecurity

--- WSClientExample/src/main/java/ws/client/example/ExampleClient.java
+++ WSClientExample/src/main/java/ws/client/example/ExampleClient.java
@@ -3,14 +3,23 @@ package ws.client.example;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import javax.xml.ws.BindingProvider;
+
 import org.tempuri.Calculator;
+import org.tempuri.CalculatorSoap;
 
 public class ExampleClient {
 
+	private String user = "";
+	private String password = "";
+
 	public void run() throws MalformedURLException {
-		Calculator service;
-		service = new Calculator(new URL("http://www.dneonline.com/calculator.asmx?WSDL"));
-		System.out.println(service.getCalculatorSoap().add(12, 12));
+		// erstellt client
+		CalculatorSoap calculatorSoap =  new Calculator(new URL("http://www.dneonline.com/calculator.asmx?WSDL")).getCalculatorSoap();
+		// fuegt die security hinzu mit user und pw
+		calculatorSoap = (CalculatorSoap) WSSecurityHeaderHelper.addWSSecurity((BindingProvider) calculatorSoap, user, password);
+		// ausfuehrung
+		System.out.println(calculatorSoap.add(12, 12));
 	}
 
 }
