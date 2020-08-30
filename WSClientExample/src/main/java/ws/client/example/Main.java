beab5c34942ddb7754d8f5bf9bf44a130d8ad3ed
krausg <guenter.kraus@mailbox.org>
2020-08-30 02:43:08 +0200

    entfernt warnung von SAAJMetaFactory

--- WSClientExample/src/main/java/ws/client/example/Main.java
+++ WSClientExample/src/main/java/ws/client/example/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
 	
 	public static void main(String[] args) throws MalformedURLException {
+		System.setProperty("javax.xml.soap.SAAJMetaFactory", "com.sun.xml.messaging.saaj.soap.SAAJMetaFactoryImpl");
 		new ExampleClient().run();
 	}
 
